50c6aabe265d9312c1f5f9ea43d0ad20a61ad000
Adam Blank <adam@12thfloor.com>
2024-06-18 09:38:48 -0500

    Updated to relative paths

--- Module03/docker_launch.sh
+++ Module03/docker_launch.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# Check if the user is in the Docker group
 if [[ $(id -nG) =~ "docker" ]] ; then
-    echo "$USER in Docker Group"
+    echo "$USER is in the Docker group"
 else
-  # Add the current user to the Docker group
-  echo "Adding $USER to the Docker group"
-  sudo usermod -aG docker $USER
-  echo "User added to the Docker group and restarting Docker"
-  sudo systemctl restart docker
+    # Add the current user to the Docker group
+    echo "Adding $USER to the Docker group"
+    sudo usermod -aG docker $USER
+    echo "User added to the Docker group. Restarting Docker..."
+    sudo systemctl restart docker
 fi
 
-#prompt if we should rebuild the container
+# Get the current path
+currentPath=$(pwd)
+
+# Prompt if we should rebuild the container
 read -p "Do you want to rebuild the container? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo "Rebuilding the container"
-    docker build . -t module03 --no-cache
+    docker build "$currentPath" -t module03 --no-cache
 fi
-docker run -it --rm --gpus=all -p 8888:8888 -p 5000:5000 -v /home/adam/GitRepos/ITAI-3377/Module03/notebooks:/root/notebooks module03
+
+# Run the Docker container with the dynamic path
+docker run -it --rm --gpus=all -p 8888:8888 -p 5000:5000 -v "$currentPath/notebooks:/root/notebooks" module03
