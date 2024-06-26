33642ba7b65428b5bef0ec83949382fd4cf94bcf
Adam Blank <adam@12thfloor.com>
2024-06-26 12:24:26 -0500

    remove no-cache option

--- Module03/docker_launch.sh
+++ Module03/docker_launch.sh
@@ -20,7 +20,7 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo "Rebuilding the container"
-    docker build "$currentPath" -t module03 --no-cache
+    docker build "$currentPath" -t module03
 fi
 
 # Run the Docker container with the dynamic path
