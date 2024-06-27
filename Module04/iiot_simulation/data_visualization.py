4fff1a89aafd657d9ad9b026754b14fd4307204e
Adam Blank <adam@12thfloor.com>
2024-06-26 23:34:50 -0500

    Trying global variable, but having issues with maptlot

--- Module04/iiot_simulation/data_visualization.py
+++ Module04/iiot_simulation/data_visualization.py
@@ -1,41 +1,63 @@
-import paho.mqtt.client as mqtt 
+import paho.mqtt.client as mqtt
 import pandas as pd 
 import matplotlib.pyplot as plt 
 from datetime import datetime 
+import time
 
-data = [] 
+
+data = []
+df = pd.DataFrame(columns=["timestamp", "temperature", "humidity"])  # Define globally scoped DataFrame
 
 
-# Added to get additional information for troubleshooting the connection
+def update_plot():
+    if not df.empty:
+        plt.clf()
+        plt.plot(df["timestamp"], df["temperature"], label="Temperature")
+        plt.plot(df["timestamp"], df["humidity"], label="Humidity")
+        plt.legend()
+        plt.draw()
+        plt.pause(0.1)
+        plt.show()
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
+    client.subscribe("sensor/data")
 
-
-def on_message(client, userdata, message): 
-    print("Message received")
-    payload = str(message.payload.decode())
-    data.append((datetime.now(), payload)) 
+def on_message(client, userdata, msg):
+    print(f"Received '{msg.payload.decode()}' from '{msg.topic}'")
+    payload = str(msg.payload.decode())
+    data.append((datetime.now(), payload))
     print("Checking Data size: ", len(data))
-    if len(data) > 100: 
-        data.pop(0) 
-        df = pd.DataFrame(data, columns=["timestamp", "sensor_data"]) 
-        df["temperature"] = df["sensor_data"].apply(lambda x: eval(x)["temperature"]) 
+    # nothing would happen in the provided code until there were 100+ messages
+    if len(data) > 10:
+        #data.pop(0)
+        df = pd.DataFrame(data, columns=["timestamp", "sensor_data"])
+        df["temperature"] = df["sensor_data"].apply(lambda x: eval(x)["temperature"])                                
         df["humidity"] = df["sensor_data"].apply(lambda x: eval(x)["humidity"]) 
-        plt.clf() 
-        plt.plot(df["timestamp"], df["temperature"], label="Temperature") 
-        plt.plot(df["timestamp"], df["humidity"], label="Humidity") 
-        plt.legend() 
-        plt.draw() 
-        plt.pause(0.1) 
+        print("Update Plot")
+        update_plot() #updates the plot with the new data
 
-client = mqtt.Client() 
+
+client = mqtt.Client()
 client.on_connect = on_connect
-client.on_message = on_message 
+client.on_message = on_message
 
-client.connect("localhost", 1883, 60) 
-client.subscribe("sensor/data") 
-client.loop_start() 
+client.connect("localhost", 1883)
 
-#plt.ion() 
-#plt.figure() 
-#plt.show()
+
+
+
+client.loop_start()
+
+plt.ion()
+plt.figure()
+plt.show()
+
+
+
+# Run for some time to receive messages
+time.sleep(200)
+
+
+client.loop_stop()  # Stop the loop
+client.disconnect()  # Disconnect from the broker
